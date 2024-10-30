ab7fbce738bbed0d8be4159c4425dfd8295e4273
ThePrimeagen <the.primeaegen@aol.com>
2024-10-30 15:43:51 -0400

    pygame init, display, and quit function

---
+++ main.py
@@ -0,0 +1,21 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+import pygame.constants
+
+def main():
+	pygame.init
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	print("Starting asteroids!")
+	print(f"Screen width: {SCREEN_WIDTH}")
+	print(f"Screen height: {SCREEN_HEIGHT}")
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+		pygame.Surface.fill(screen, (0,0,0))
+		pygame.display.flip()
+if __name__ == "__main__":
+	main()
