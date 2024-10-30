0cfe81123afbf0c9fbea3c25e15ef0dccc3a7f33
ThePrimeagen <the.primeaegen@aol.com>
2024-10-30 16:34:44 -0400

    adding player triangle and hitbox into center of screen

--- main.py
+++ main.py
@@ -4,18 +4,27 @@
 import pygame
 from constants import *
 import pygame.constants
+from player import *
 
 def main():
 	pygame.init
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+		
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		pygame.Surface.fill(screen, (0,0,0))
+		player.draw(screen)
 		pygame.display.flip()
+		clock.tick(60)
+		dt = clock.tick() / 1000
 if __name__ == "__main__":
 	main()
