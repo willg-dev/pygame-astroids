f04412a3ce8b58a3d8b87dcc2d3c3b6d09f63df9
ThePrimeagen <the.primeaegen@aol.com>
2024-10-31 12:45:19 -0400

    Adding movement, asteroids, asteroid spawning, and shooting

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+import sys
 import pygame
 from constants import *
-import pygame.constants
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
-	pygame.init
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock = pygame.time.Clock()
-	dt = 0
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-		
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-	while True:
-		dt = clock.tick() / 1000
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-		pygame.Surface.fill(screen, (0,0,0))
-		player.update(dt)
-		player.draw(screen)
-		pygame.display.flip()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 if __name__ == "__main__":
-	main()
+    main()
